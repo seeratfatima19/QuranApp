324964d6596c9fa21438b2a748ba263a95e67fdb
seeratfatima19 <seeratfatima991@yahoo.com>
2023-06-02 16:53:34 +0500

    initial commit

---
+++ app/src/main/java/com/example/quranmajeed/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.quranmajeed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(MainActivity.this,QuranActivity.class);
+                MainActivity.this.startActivity(mainIntent);
+                MainActivity.this.finish();
+            }
+        },3000);
+    }
+}
